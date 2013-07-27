fca592228d498cdfeaf2dbddc9677eeb7c16f104
Ubuntu <ubuntu@ip-10-179-5-144.ec2.internal>
2013-07-27 09:32:06 +0000

    Modified index.html to add stylesheet links in <head>

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 var data = fs.readFileSync("index.html", "utf-8");
 
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
